a2ebb8c61386d826b7d9107dd5b7fa98c1abfa6f
Jacob Good <jgood@good-it-solutions.com>
2020-07-15 12:00:14 -0400

    fixed an issue where client would break using BrowserRouter on refresh.

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
